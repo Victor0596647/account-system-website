498f6940845018cd92bda6a7f9eff36d91081727
Victor0596647 <85453971+Victor0596647@users.noreply.github.com>
2022-04-17 03:47:44 -0700

    changed connection

--- connection.php
+++ connection.php
@@ -3,16 +3,8 @@ $dbhost = getenv("MYSQL_ADDON_HOST");
 $dbuser = getenv("MYSQL_ADDON_USER");
 $dbpass = getenv("MYSQL_ADDON_PASSWORD");
 $dbname = getenv("MYSQL_ADDON_DB");
-$con = NULL;
-try{
-    $con = new PDO(
-        "mysql:host=$dbhost; dbname=$dbname",
-        "$dbuser",
-        "$dbpass"
-    );
-    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected!";
-}catch(PDOException $e){
-    die("something went wrong...");
+$con = new mysqli($dbhost,$dbuser,$dbpass,$dbname);
+if(!$con){
+    die("something went wrong");
 }
 ?>
