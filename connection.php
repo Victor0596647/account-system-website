83d5fa4f80fbc55cb1529cfc734dd07da751cbcd
Victor0596647 <85453971+Victor0596647@users.noreply.github.com>
2022-04-17 03:37:15 -0700

    changed sql

--- connection.php
+++ connection.php
@@ -1,12 +1,18 @@
 <?php
-$dbhost = '127.0.0.1';
-$dbuser = 'root';
-$dbpass = '';
-$dbname = 'php_dev';
-$con = new mysqli($dbhost,$dbuser,$dbpass,$dbname);
-
-if(!$con){
-    die('Failed to connect!');
+$dbhost = getenv("MYSQL_ADDON_HOST");
+$dbuser = getenv("MYSQL_ADDON_USER");
+$dbpass = getenv("MYSQL_ADDON_PASSWORD");
+$dbname = getenv("MYSQL_ADDON_DB");
+$con = NULL;
+try{
+    $con = new PDO(
+        "mysql:host=$dbhost; dbname=$dbname",
+        "$dbuser",
+        "$dbpass"
+    );
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected!";
+}catch(PDOException $e){
+    die("something went wrong...");
 }
-
 ?>
