c82e3369263b9f04bd8e0b2b98e21aa9ba82a209
Victor0596647 <85453971+Victor0596647@users.noreply.github.com>
2022-04-21 20:00:14 +0000

    pop up

--- index.php
+++ index.php
@@ -18,8 +18,8 @@
             <form action="index.php" method="post">
               <h4>Do you want to remove this account from the database?</h4>
               <input type="submit" name="choice" value="Yes">
+              <input type="submit" name="choice" value="No" onclick="modal.style.display = 'none';">
             </form>
-            <input type="button" value="No" onclick="modal.style.display = 'none';">
             <?php
               if(isset($_POST['choice'])){
                 if($_POST['choice'] == "Yes"){
@@ -38,7 +38,7 @@
         <h3>Welcome, <?php echo $_userdata['user_fullname'];?></h3>
         <h5>Username: <?php echo $_userdata['user_name'];?></h5>
         <a href="logout.php">Log Out</a>
-        <a id="myBtn" href="">Remove Account</a>
+        <a id="myBtn">Remove Account</a>
     </body>
     <script src="user_dashboard.js"></script>
 </html>
