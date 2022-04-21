ced24da40f27a26fe9eb80322a257b229ae5223d
Victor0596647 <85453971+Victor0596647@users.noreply.github.com>
2022-04-21 19:25:04 +0000

    pop up

--- index.php
+++ index.php
@@ -9,56 +9,30 @@
 <html>
     <head>
         <title>asw | Dashboard</title>
-        <style>
-body {font-family: Arial, Helvetica, sans-serif;}
-
-/* The Modal (background) */
-.modal {
-  display: none; /* Hidden by default */
-  position: fixed; /* Stay in place */
-  z-index: 1; /* Sit on top */
-  padding-top: 100px; /* Location of the box */
-  left: 0;
-  top: 0;
-  width: 100%; /* Full width */
-  height: 100%; /* Full height */
-  overflow: auto; /* Enable scroll if needed */
-  background-color: rgb(0,0,0); /* Fallback color */
-  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
-}
-
-/* Modal Content */
-.modal-content {
-  background-color: #fefefe;
-  margin: auto;
-  padding: 20px;
-  border: 1px solid #888;
-  width: 80%;
-}
-
-/* The Close Button */
-.close {
-  color: #aaaaaa;
-  float: right;
-  font-size: 28px;
-  font-weight: bold;
-}
-
-.close:hover,
-.close:focus {
-  color: #000;
-  text-decoration: none;
-  cursor: pointer;
-}
-</style>
+        <link rel="stylesheet" type="text/css" href="user_dashboard.css">
     </head>
     <body>
         <div id="myModal" class="modal">
 
         <!-- Modal content -->
         <div class="modal-content">
-        <span class="close">&times;</span>
-        <p>Some text in the Modal..</p>
+          <span class="close">&times;</span>
+          <form action="index.php" method="post">
+            <h4>Do you want to remove this account from the database?</h4>
+            <input type="submit" name="choice" value="yes" placeholder="Yes">
+            <input type="submit" name="choice" value="no" placeholder="No" onclick="modal.style.display = 'none';">
+          </form>
+          <?php
+            if(isset($_POST['choice'])){
+              if($_POST['choice'] == "yes"){
+                  $query = "DELETE FROM user_accounts WHERE user_id = '".$_SESSION['user_id']."'";
+                  mysqli_query($con,$query);
+                  session_destroy();
+                  header("location: login.php");
+                  die;
+              }
+            }
+          ?>
         </div>
 
         </div>
@@ -69,32 +43,5 @@ body {font-family: Arial, Helvetica, sans-serif;}
         <a href="logout.php">Log Out</a>
         <a id="myBtn">Remove Account</a>
     </body>
-
-<script>
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-</script>
+    <script src="user_dashboard.js"></script>
 </html>
