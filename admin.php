9698892e834d0f88c4f54242488cf670acad6728
Victor0596647 <85453971+Victor0596647@users.noreply.github.com>
2022-04-23 10:47:57 +0000

    fix admin

--- admin.php
+++ admin.php
@@ -8,11 +8,12 @@
     if($_userdata['privileges'] != "admin"){
         header("location: index.php");
         die;
-    }else{
-        $query = "select user_id,user_name,user_password,user_fullname,date from user_accounts order by date";
-        $result = mysqli_query($con, $query);
-        $users = mysqli_fetch_all($result,MYSQLI_ASSOC);
     }
+
+    //User informations
+    $query = "select user_id,user_name,user_password,user_fullname,date from user_accounts order by date";
+    $result = mysqli_query($con, $query);
+    $users = mysqli_fetch_all($result,MYSQLI_ASSOC);
 ?>
 <!DOCTYPE html>
 <html>
